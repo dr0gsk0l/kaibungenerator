a8e39ddf4302f32f3411a7eb1301e372d9a48e39
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2020-04-17 19:58:00 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -9,6 +9,10 @@ var MATO='<img src="https://3.bp.blogspot.com/-OBwWX1xu1xc/WGnPb8Ly_HI/AAAAAAABA
 var TO='<img src="https://2.bp.blogspot.com/-6noe-4G5FNU/V2ubhr0UDKI/AAAAAAAA7pY/nHp2zGXHshkiwzUxYa5RQVf-pcwn31GSACLcB/s800/door_half_open.png" alt="戸" width="50px" height="100px">';
 var MA='<img src="https://3.bp.blogspot.com/-m31LpaTSzy4/U32NRZQpjbI/AAAAAAAAgr0/xx3csFTzTJ8/s800/character_akuma.png" alt="悪魔" width="60px" height="100px">';
 document.getElementById("to").onclick=function(){
+  if(ansimg.length){
+    ansimg="";
+    ansstr="";
+  }
   console.log("と");
   ansstr+='と';
   document.getElementById("ansstring").innerText=ansstr;
@@ -16,6 +20,10 @@ document.getElementById("to").onclick=function(){
 }
 
 document.getElementById("ma").onclick=function(){
+  if(ansimg.length){
+    ansimg="";
+    ansstr="";
+  }
   console.log("ま");
   ansstr+='ま';
   document.getElementById("ansstring").innerText=ansstr;
