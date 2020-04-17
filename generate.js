4456c9e69ff81c5e82316855e08486a72292e6fe
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2020-04-17 22:54:28 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -5,7 +5,10 @@ const tweetDivided = document.getElementById('tweet');
 document.getElementById("ansstring").innerText="ボタンを押してね";
 
 var TOMATO='<img src="https://4.bp.blogspot.com/-FlkEDz3wQSc/Vtw1wrbo6AI/AAAAAAAA4fk/k8tfPNkMvb4/s800/tomato_red.png" alt="トマト" width="100px" height="100px">';
+var TOMATO2='<img src="https://2.bp.blogspot.com/-i3xPt3TISb0/UWgWlyylbUI/AAAAAAAAQGo/JUtkdKSkloc/s1600/vegetable_tomato.png" alt="トマト2" width="120px" height="100px">';
 var MAMA='<img src="https://3.bp.blogspot.com/-Tsx2ag1qcVk/UNO5bQwAElI/AAAAAAAAIx8/jwOIg9WO8L8/s1600/apron_mama.png" alt="ママ" width="50px" height="100px">';
+var MAMA2='<img src="https://3.bp.blogspot.com/-x2DitymfGAo/UnyHS2DYEZI/AAAAAAAAahQ/HDhiTG4b4n8/s800/cooking_mama.png" alt="ママ2" width="100px" height="100px">';
+var MAMA3='<img src="https://2.bp.blogspot.com/-M28y-PtBkHM/Uj_28pHhwWI/AAAAAAAAYE0/9HYKZ4HErwE/s800/mother_angry.png" alt="ママ3" width="90px" height="100px">';
 var MATO='<img src="https://3.bp.blogspot.com/-OBwWX1xu1xc/WGnPb8Ly_HI/AAAAAAABA60/Qu-WGOBcSicVM8iSXq2xxXQcpRZE7zyWQCLcB/s800/sports_archery_mato1.png" alt="的" width="100px" height="100px">';
 var TO='<img src="https://2.bp.blogspot.com/-6noe-4G5FNU/V2ubhr0UDKI/AAAAAAAA7pY/nHp2zGXHshkiwzUxYa5RQVf-pcwn31GSACLcB/s800/door_half_open.png" alt="戸" width="50px" height="100px">';
 var MA='<img src="https://3.bp.blogspot.com/-m31LpaTSzy4/U32NRZQpjbI/AAAAAAAAgr0/xx3csFTzTJ8/s800/character_akuma.png" alt="悪魔" width="60px" height="100px">';
@@ -55,7 +58,8 @@ document.getElementById("make").onclick=function(){
     if(ansstr.substr(i,3)=='とまと'){
       if(meisi)tmp+='，';
       tmp+='トマト';
-      ansimg+=TOMATO+' ';
+      if(i%17==6)ansimg+=TOMATO2+' ';
+      else ansimg+=TOMATO+' ';
       i+=3;
       meisi=true;
       continue;
@@ -63,7 +67,9 @@ document.getElementById("make").onclick=function(){
     if(ansstr.substr(i,2)=='まま'){
       if(meisi)tmp+='，';
       tmp+='ママ';
-      ansimg+=MAMA+' ';
+      if(i%33==0)ansimg+=MAMA2+' ';
+      else if(i%33==7)ansimg+=MAMA3+' ';
+      else ansimg+=MAMA+' ';
       i+=2;
       meisi=true;
       continue;
