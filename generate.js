fcab7dfb5d0677fb2a2d78e9817fbc283948deba
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2021-03-14 17:28:43 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,4 +1,4 @@
-/*var ansstr="";
+var ansstr="";
 var ansimg="";
 var meisi=false;
 const tweetDivided = document.getElementById('tweet');
@@ -124,4 +124,3 @@ document.getElementById("make").onclick=function(){
   ansimg+="あ";
   meisi=false;
 }
-*/
