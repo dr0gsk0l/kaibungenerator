2c7d382a2561d201a80001b27e2cd81068c70684
dr0gsk0l <44800800+dr0gsk0l@users.noreply.github.com>
2021-03-14 16:40:21 +0900

    Update images.js

--- images.js
+++ images.js
@@ -1,54 +1,54 @@
-var TOMATO='<img src="https://4.bp.blogspot.com/-FlkEDz3wQSc/Vtw1wrbo6AI/AAAAAAAA4fk/k8tfPNkMvb4/s800/tomato_red.png" alt="トマト"  width=auto height="14%">';
-var TOMATO2='<img src="https://2.bp.blogspot.com/-i3xPt3TISb0/UWgWlyylbUI/AAAAAAAAQGo/JUtkdKSkloc/s1600/vegetable_tomato.png" alt="トマト2" height="14%">';
-var MAMA='<img src="https://3.bp.blogspot.com/-Tsx2ag1qcVk/UNO5bQwAElI/AAAAAAAAIx8/jwOIg9WO8L8/s1600/apron_mama.png" alt="ママ" width=auto height="14%">';
-var MAMA2='<img src="https://3.bp.blogspot.com/-x2DitymfGAo/UnyHS2DYEZI/AAAAAAAAahQ/HDhiTG4b4n8/s800/cooking_mama.png" alt="ママ2" height="14%">';
-var MAMA3='<img src="https://2.bp.blogspot.com/-M28y-PtBkHM/Uj_28pHhwWI/AAAAAAAAYE0/9HYKZ4HErwE/s800/mother_angry.png" alt="ママ3" width=auto height="14%">';
-var MATO='<img src="https://3.bp.blogspot.com/-OBwWX1xu1xc/WGnPb8Ly_HI/AAAAAAABA60/Qu-WGOBcSicVM8iSXq2xxXQcpRZE7zyWQCLcB/s800/sports_archery_mato1.png" alt="的" width=auto height="14%">';
-var TO='<img src="https://2.bp.blogspot.com/-6noe-4G5FNU/V2ubhr0UDKI/AAAAAAAA7pY/nHp2zGXHshkiwzUxYa5RQVf-pcwn31GSACLcB/s800/door_half_open.png" alt="戸" width=auto height="14%">';
-var MA='<img src="https://3.bp.blogspot.com/-m31LpaTSzy4/U32NRZQpjbI/AAAAAAAAgr0/xx3csFTzTJ8/s800/character_akuma.png" alt="悪魔" width=auto height="14%">';
-var HITOME='<img src="https://3.bp.blogspot.com/-9_vm6fBg8dw/V_4bzTx9OaI/AAAAAAAA-vc/7H75GLosPTYvV8Nd-K5zGTtviYzZQpFMwCLcB/s800/pose_nozoku_blind.png" alt="人目" width=auto height="14%">';
-var HITOMATOME='<img src="https://3.bp.blogspot.com/-pF9-RWRttnE/UV1JHjxu-dI/AAAAAAAAPTY/roqGEIUYZRA/s450/key.png" alt="ひとまとめ" width=auto height="14%">';
-var MATOME='<img src="https://4.bp.blogspot.com/-trtHL6GrztE/WWXXWKOmVDI/AAAAAAABFhk/75aMZKZCReErCPiZenZpT00mZvNjuV6XgCLcBGAs/s800/website_matome_site.png" alt="まとめ" width=auto height="14%">';
-var HIHI='<img src="https://pbs.twimg.com/media/EWCfhqYUEAEwuIu?format=png&name=small" alt="ヒヒ" width=auto height="14%">';
-var HIME='<img src="https://1.bp.blogspot.com/-dQHPshzm_tY/WaOePIs3e9I/AAAAAAABGLI/bvasBK5wZPIVPskCglfUp5itLsFfJJRGQCLcBGAs/s180-c/royal_princess.png" alt="姫" width=auto height="14%">';
-var MAME='<img src="https://1.bp.blogspot.com/-UfAoUzcJCMs/Udy6oyQbj6I/AAAAAAAAWKQ/bD4waahGR6I/s400/food_endoumame.png" alt="豆" width=auto height="14%">';
-var HITO='<img src="https://1.bp.blogspot.com/-sT0aNSDSNd8/W6DT3AoIEtI/AAAAAAABO_4/OH0B97M-Hu8lyzYFM6V1m-z0qCx-WV_bgCLcBGAs/s800/talk4_green_man.png" alt="人" width=auto height="14%">';
-var HITO2='<img src="https://1.bp.blogspot.com/-BU_JEXiwX-U/XRHH8c8Se3I/AAAAAAABTYU/i1I_GS0HawoaiktSasQM5M5tbb1Z_NgaQCLcBGAs/s800/ryuugakusei_daigaku_black_man.png" alt="人" width=auto height="14%">';
-var HITO3='<img src="https://4.bp.blogspot.com/-3ucwJP0Yc3Y/W6DT4kRm1BI/AAAAAAABPAI/FY6w2WWEVQcleBVoNKXWGGFUNVBfB9LkwCLcBGAs/s800/talk9_brown_man.png" alt="人" width=auto height="14%">';
-var HITO4='<img src="https://1.bp.blogspot.com/-dgtkP4wAOnk/W6DT38rTvOI/AAAAAAABPAA/fvIweeOxx9AfkNTFQ3OOKXzcPXqlzH7VACLcBGAs/s800/talk7_blue_man.png" alt="人" width=auto height="14%">';
-var HITO5='<img src="https://1.bp.blogspot.com/-HqzQ05aA7xE/XWS5iiw_2uI/AAAAAAABUTs/VJuekgkFXDU2PLpdQYnXNrce42U9nY0rwCLcBGAs/s1600/kakedasu_people2.png" alt="人" width=auto height="14%">';
-var HIMA='<img src="https://3.bp.blogspot.com/-fImkQ-EyglU/VJF_oRM5p4I/AAAAAAAAp5I/N4jypFga8ew/s400/hima_taikutsu.png" alt="暇" width=auto height="14%">';
-var MAHI='<img src="https://4.bp.blogspot.com/-354gcMIxGnE/WYAxp9UjnxI/AAAAAAABFuc/uzU1pPFBlwkcq47izNs9UnP9_7SvRIYeQCLcBGAs/s800/body_mahi_man.png" alt="麻痺" width=auto height="14%">';
-var HI='<img src="https://3.bp.blogspot.com/-bTpI2R-Kxe0/Viio_KII7WI/AAAAAAAAztM/oNokVj_uJyI/s400/honoo_hi_fire.png" alt="火" width=auto height="14%">';
-var ME='<img src="https://2.bp.blogspot.com/-UNeJcdfWzIw/Wn1ZZObzwbI/AAAAAAABKL0/pyLhnj-bz8clSvYGOKpCNvZ_4kXxoXJcgCLcBGAs/s800/body_eye_color9_black.png" alt="目" width=auto height="14%">';
-var SHINBUNSHI='<img src="https://3.bp.blogspot.com/--hFzOAXOk-Y/Wn1aH38lQvI/AAAAAAABKSQ/bFMS6Hoae8kQpwi2y4SzdEBScWCC7j6RACLcBGAs/s400/news_shinbunshi.png" alt="新聞紙" width=auto height="14%">';
-var MAMATOHITO='<img src="https://2.bp.blogspot.com/-c27iEQIizZo/V2ucndSaVhI/AAAAAAAA70w/PFSPB_npLCoULgUUn4ltSWAsLoASMN54gCLcB/s800/uwaki_genba_woman.png" alt="ママと人" width=auto height="14%">';
-var MASHINE='<img src="https://1.bp.blogspot.com/-A4669A-c7Iw/Vq89EUvfqdI/AAAAAAAA3lY/N7Uzmc-426k/s400/machine_yubisashi_kosyou.png" alt="マシン" width=auto height="14%">';
-var BUNSHI='<img src="https://3.bp.blogspot.com/-Ac8SaHcmlMA/WIW-Ok_GUjI/AAAAAAABBS0/BoAOJqIHeco19fiET1tN2tF3NJ_tv_Z7wCLcB/s400/kagaku_bunshi.png" alt="分子" width=auto height="14%">';
-var HITONANA='<img src="https://1.bp.blogspot.com/-9pDsJGXvNk4/XWS5fc5quiI/AAAAAAABUS8/xACX0wOId9U_NxqL80sNEcL96Bx_Qs4tQCLcBGAs/s1600/group_family_asia.png" alt="七人" width=auto height="14%">';
-var SHIMA='<img src="https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png" alt="島" width=auto height="14%">';
-var KA='<img src="https://4.bp.blogspot.com/-7N6zGrdV09E/WBLNCp0SXBI/AAAAAAAA_Mg/_zgTCeSHvpQdphodHKiymXgzkW-6tdYzACLcB/s800/bug_ka.png" alt="蚊" width=auto height="14%">';
-var SHIKA1='<img src="https://1.bp.blogspot.com/-bu6i-g8TaeI/UX-OTyfTR3I/AAAAAAAAQ2o/NPdroBrw-ac/s1600/animal_deer.png" alt="鹿" width=auto height="14%">';
-var SHIKA2='<img src="https://4.bp.blogspot.com/-QhV91eEE8ms/UZ2U537QLOI/AAAAAAAATpk/n5gGu_PitTY/s800/haisya.png" alt="歯科" width=auto height="14%">';
-var KAMA1='<img src="https://3.bp.blogspot.com/-Ec7JsW5NYBg/VOsJXK4U7RI/AAAAAAAArmo/nIFlmTcoaUA/s800/kama.png" alt="鎌" width=auto height="14%">';
-var KAMA2='<img src="https://3.bp.blogspot.com/-C6DZSoNmdas/WvQH4ZxYUlI/AAAAAAABL-Y/0gluL04Pn-UgONpZ-DJYynqA6-GNPzb_gCLcBGAs/s800/syokki_kamameshi_close.png" alt="釜" width=auto height="14%">';
-var KAME1='<img src="https://4.bp.blogspot.com/-Noj-Uad0wo4/V5NEVnfiP_I/AAAAAAAA8iY/RxBWA_TTLP4KvWDppYucUb5Et55qgp5YQCLcB/s800/animal_umigame_osagame.png" alt="亀" width=auto height="14%">';
-var KAME2='<img src="https://2.bp.blogspot.com/-HcZzWmNl0VY/V5NEVoNt9QI/AAAAAAAA8ic/KPAKh_jhcZsFetZFttp-XCe4oSHfqwDrACLcB/s800/animal_umigame_aka.png" alt="亀" width=auto height="14%">';
-var KAME3='<img src="https://4.bp.blogspot.com/-YXZgbQRViEM/V5NSA2-1k3I/AAAAAAAA8lc/OqYNLGApMqccbG_OjFuoUtMArqcfUrvjgCLcB/s800/animal_umigame_ao.png" alt="亀" width=auto height="14%">';
-var KAME4='<img src="https://3.bp.blogspot.com/-rS1w3AlRQ9w/V5NEX0ps7gI/AAAAAAAA8is/QYYrat7X_GUkdWwlUkvin8P_ZWtOjBVlgCLcB/s800/animal_umigame_taimai.png" alt="亀" width=auto height="14%">';
-var SECRET='<img src="https://pbs.twimg.com/media/Ef3GTYXU4AAQFno?format=png&name=small" alt="secret" width=auto height="50%">';
+var TOMATO='<img src="https://4.bp.blogspot.com/-FlkEDz3wQSc/Vtw1wrbo6AI/AAAAAAAA4fk/k8tfPNkMvb4/s800/tomato_red.png" alt="トマト"  width=auto height="14vw">';
+var TOMATO2='<img src="https://2.bp.blogspot.com/-i3xPt3TISb0/UWgWlyylbUI/AAAAAAAAQGo/JUtkdKSkloc/s1600/vegetable_tomato.png" alt="トマト2" height="14vw">';
+var MAMA='<img src="https://3.bp.blogspot.com/-Tsx2ag1qcVk/UNO5bQwAElI/AAAAAAAAIx8/jwOIg9WO8L8/s1600/apron_mama.png" alt="ママ" width=auto height="14vw">';
+var MAMA2='<img src="https://3.bp.blogspot.com/-x2DitymfGAo/UnyHS2DYEZI/AAAAAAAAahQ/HDhiTG4b4n8/s800/cooking_mama.png" alt="ママ2" height="14vw">';
+var MAMA3='<img src="https://2.bp.blogspot.com/-M28y-PtBkHM/Uj_28pHhwWI/AAAAAAAAYE0/9HYKZ4HErwE/s800/mother_angry.png" alt="ママ3" width=auto height="14vw">';
+var MATO='<img src="https://3.bp.blogspot.com/-OBwWX1xu1xc/WGnPb8Ly_HI/AAAAAAABA60/Qu-WGOBcSicVM8iSXq2xxXQcpRZE7zyWQCLcB/s800/sports_archery_mato1.png" alt="的" width=auto height="14vw">';
+var TO='<img src="https://2.bp.blogspot.com/-6noe-4G5FNU/V2ubhr0UDKI/AAAAAAAA7pY/nHp2zGXHshkiwzUxYa5RQVf-pcwn31GSACLcB/s800/door_half_open.png" alt="戸" width=auto height="14vw">';
+var MA='<img src="https://3.bp.blogspot.com/-m31LpaTSzy4/U32NRZQpjbI/AAAAAAAAgr0/xx3csFTzTJ8/s800/character_akuma.png" alt="悪魔" width=auto height="14vw">';
+var HITOME='<img src="https://3.bp.blogspot.com/-9_vm6fBg8dw/V_4bzTx9OaI/AAAAAAAA-vc/7H75GLosPTYvV8Nd-K5zGTtviYzZQpFMwCLcB/s800/pose_nozoku_blind.png" alt="人目" width=auto height="14vw">';
+var HITOMATOME='<img src="https://3.bp.blogspot.com/-pF9-RWRttnE/UV1JHjxu-dI/AAAAAAAAPTY/roqGEIUYZRA/s450/key.png" alt="ひとまとめ" width=auto height="14vw">';
+var MATOME='<img src="https://4.bp.blogspot.com/-trtHL6GrztE/WWXXWKOmVDI/AAAAAAABFhk/75aMZKZCReErCPiZenZpT00mZvNjuV6XgCLcBGAs/s800/website_matome_site.png" alt="まとめ" width=auto height="14vw">';
+var HIHI='<img src="https://pbs.twimg.com/media/EWCfhqYUEAEwuIu?format=png&name=small" alt="ヒヒ" width=auto height="14vw">';
+var HIME='<img src="https://1.bp.blogspot.com/-dQHPshzm_tY/WaOePIs3e9I/AAAAAAABGLI/bvasBK5wZPIVPskCglfUp5itLsFfJJRGQCLcBGAs/s180-c/royal_princess.png" alt="姫" width=auto height="14vw">';
+var MAME='<img src="https://1.bp.blogspot.com/-UfAoUzcJCMs/Udy6oyQbj6I/AAAAAAAAWKQ/bD4waahGR6I/s400/food_endoumame.png" alt="豆" width=auto height="14vw">';
+var HITO='<img src="https://1.bp.blogspot.com/-sT0aNSDSNd8/W6DT3AoIEtI/AAAAAAABO_4/OH0B97M-Hu8lyzYFM6V1m-z0qCx-WV_bgCLcBGAs/s800/talk4_green_man.png" alt="人" width=auto height="14vw">';
+var HITO2='<img src="https://1.bp.blogspot.com/-BU_JEXiwX-U/XRHH8c8Se3I/AAAAAAABTYU/i1I_GS0HawoaiktSasQM5M5tbb1Z_NgaQCLcBGAs/s800/ryuugakusei_daigaku_black_man.png" alt="人" width=auto height="14vw">';
+var HITO3='<img src="https://4.bp.blogspot.com/-3ucwJP0Yc3Y/W6DT4kRm1BI/AAAAAAABPAI/FY6w2WWEVQcleBVoNKXWGGFUNVBfB9LkwCLcBGAs/s800/talk9_brown_man.png" alt="人" width=auto height="14vw">';
+var HITO4='<img src="https://1.bp.blogspot.com/-dgtkP4wAOnk/W6DT38rTvOI/AAAAAAABPAA/fvIweeOxx9AfkNTFQ3OOKXzcPXqlzH7VACLcBGAs/s800/talk7_blue_man.png" alt="人" width=auto height="14vw">';
+var HITO5='<img src="https://1.bp.blogspot.com/-HqzQ05aA7xE/XWS5iiw_2uI/AAAAAAABUTs/VJuekgkFXDU2PLpdQYnXNrce42U9nY0rwCLcBGAs/s1600/kakedasu_people2.png" alt="人" width=auto height="14vw">';
+var HIMA='<img src="https://3.bp.blogspot.com/-fImkQ-EyglU/VJF_oRM5p4I/AAAAAAAAp5I/N4jypFga8ew/s400/hima_taikutsu.png" alt="暇" width=auto height="14vw">';
+var MAHI='<img src="https://4.bp.blogspot.com/-354gcMIxGnE/WYAxp9UjnxI/AAAAAAABFuc/uzU1pPFBlwkcq47izNs9UnP9_7SvRIYeQCLcBGAs/s800/body_mahi_man.png" alt="麻痺" width=auto height="14vw">';
+var HI='<img src="https://3.bp.blogspot.com/-bTpI2R-Kxe0/Viio_KII7WI/AAAAAAAAztM/oNokVj_uJyI/s400/honoo_hi_fire.png" alt="火" width=auto height="14vw">';
+var ME='<img src="https://2.bp.blogspot.com/-UNeJcdfWzIw/Wn1ZZObzwbI/AAAAAAABKL0/pyLhnj-bz8clSvYGOKpCNvZ_4kXxoXJcgCLcBGAs/s800/body_eye_color9_black.png" alt="目" width=auto height="14vw">';
+var SHINBUNSHI='<img src="https://3.bp.blogspot.com/--hFzOAXOk-Y/Wn1aH38lQvI/AAAAAAABKSQ/bFMS6Hoae8kQpwi2y4SzdEBScWCC7j6RACLcBGAs/s400/news_shinbunshi.png" alt="新聞紙" width=auto height="14vw">';
+var MAMATOHITO='<img src="https://2.bp.blogspot.com/-c27iEQIizZo/V2ucndSaVhI/AAAAAAAA70w/PFSPB_npLCoULgUUn4ltSWAsLoASMN54gCLcB/s800/uwaki_genba_woman.png" alt="ママと人" width=auto height="14vw">';
+var MASHINE='<img src="https://1.bp.blogspot.com/-A4669A-c7Iw/Vq89EUvfqdI/AAAAAAAA3lY/N7Uzmc-426k/s400/machine_yubisashi_kosyou.png" alt="マシン" width=auto height="14vw">';
+var BUNSHI='<img src="https://3.bp.blogspot.com/-Ac8SaHcmlMA/WIW-Ok_GUjI/AAAAAAABBS0/BoAOJqIHeco19fiET1tN2tF3NJ_tv_Z7wCLcB/s400/kagaku_bunshi.png" alt="分子" width=auto height="14vw">';
+var HITONANA='<img src="https://1.bp.blogspot.com/-9pDsJGXvNk4/XWS5fc5quiI/AAAAAAABUS8/xACX0wOId9U_NxqL80sNEcL96Bx_Qs4tQCLcBGAs/s1600/group_family_asia.png" alt="七人" width=auto height="14vw">';
+var SHIMA='<img src="https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png" alt="島" width=auto height="14vw">';
+var KA='<img src="https://4.bp.blogspot.com/-7N6zGrdV09E/WBLNCp0SXBI/AAAAAAAA_Mg/_zgTCeSHvpQdphodHKiymXgzkW-6tdYzACLcB/s800/bug_ka.png" alt="蚊" width=auto height="14vw">';
+var SHIKA1='<img src="https://1.bp.blogspot.com/-bu6i-g8TaeI/UX-OTyfTR3I/AAAAAAAAQ2o/NPdroBrw-ac/s1600/animal_deer.png" alt="鹿" width=auto height="14vw">';
+var SHIKA2='<img src="https://4.bp.blogspot.com/-QhV91eEE8ms/UZ2U537QLOI/AAAAAAAATpk/n5gGu_PitTY/s800/haisya.png" alt="歯科" width=auto height="14vw">';
+var KAMA1='<img src="https://3.bp.blogspot.com/-Ec7JsW5NYBg/VOsJXK4U7RI/AAAAAAAArmo/nIFlmTcoaUA/s800/kama.png" alt="鎌" width=auto height="14vw">';
+var KAMA2='<img src="https://3.bp.blogspot.com/-C6DZSoNmdas/WvQH4ZxYUlI/AAAAAAABL-Y/0gluL04Pn-UgONpZ-DJYynqA6-GNPzb_gCLcBGAs/s800/syokki_kamameshi_close.png" alt="釜" width=auto height="14vw">';
+var KAME1='<img src="https://4.bp.blogspot.com/-Noj-Uad0wo4/V5NEVnfiP_I/AAAAAAAA8iY/RxBWA_TTLP4KvWDppYucUb5Et55qgp5YQCLcB/s800/animal_umigame_osagame.png" alt="亀" width=auto height="14vw">';
+var KAME2='<img src="https://2.bp.blogspot.com/-HcZzWmNl0VY/V5NEVoNt9QI/AAAAAAAA8ic/KPAKh_jhcZsFetZFttp-XCe4oSHfqwDrACLcB/s800/animal_umigame_aka.png" alt="亀" width=auto height="14vw">';
+var KAME3='<img src="https://4.bp.blogspot.com/-YXZgbQRViEM/V5NSA2-1k3I/AAAAAAAA8lc/OqYNLGApMqccbG_OjFuoUtMArqcfUrvjgCLcB/s800/animal_umigame_ao.png" alt="亀" width=auto height="14vw">';
+var KAME4='<img src="https://3.bp.blogspot.com/-rS1w3AlRQ9w/V5NEX0ps7gI/AAAAAAAA8is/QYYrat7X_GUkdWwlUkvin8P_ZWtOjBVlgCLcB/s800/animal_umigame_taimai.png" alt="亀" width=auto height="14vw">';
+var SECRET='<img src="https://pbs.twimg.com/media/Ef3GTYXU4AAQFno?format=png&name=small" alt="secret" width=auto height="50vw">';
 var MAJU=[
-  '<img src="https://1.bp.blogspot.com/-Zg3Rhwl3Np4/VuKENvWUmLI/AAAAAAAA4x8/ltMQ0pH0zR071jtfl8sG7mS3SQTJdh0iQ/s800/uma_chupacabra_new.png"  width=auto height="50%">',
-  '<img src="https://3.bp.blogspot.com/-GRGQEnE3UqA/UT10SkjkLMI/AAAAAAAAOvE/3vk4npeqTFg/s400/youkai_kirin.png"  width=auto height="50%">',
-  '<img src="https://3.bp.blogspot.com/-efiP_VuiSs8/WvkK6D5maMI/AAAAAAABMCY/-3dDSL_pTkc5LiuUnu0EEgs3TL4be1bpgCPcBGAYYCw/s800/fantasy_yamata_red.png"  width=auto height="50%">',
-  '<img src="https://4.bp.blogspot.com/-6p79nzYZn-Q/UT10LFSwRXI/AAAAAAAAOss/X6_1oWkCecs/s450/fantasy_yamata.png"  width=auto height="50%">',
-  '<img src="https://4.bp.blogspot.com/-gKPQ9hjH4Vs/UT10IwHX8nI/AAAAAAAAOsE/Hesj5Cuhs4o/s400/fantasy_ogre.png"  width=auto height="50%">',
-  '<img src="https://3.bp.blogspot.com/-zs4Ab_aVoac/UT10H2mvixI/AAAAAAAAOr8/skuRZVt4nDQ/s400/fantasy_goblin.png"  width=auto height="50%">',
-  '<img src="https://3.bp.blogspot.com/-i-FxBmcAJg4/UT10Kvl0MJI/AAAAAAAAOsk/knkH22IM-DQ/s400/fantasy_wolfman.png"  width=auto height="50%">',
-  '<img src="https://4.bp.blogspot.com/-yr7B9IanNcY/Wn1V29LFeVI/AAAAAAABKF4/jUBITNcwgXE4QpD4coaOyE7HngsxND2RwCLcBGAs/s400/fantasy_mandrake_mandragora.png"  width=auto height="50%">',
-  '<img src="https://3.bp.blogspot.com/-LKAxA70lNJc/WUdYUs_hdwI/AAAAAAABE6c/zIdhHJeM_k0zUGE-lrpwCOHcx2H9XPLrgCLcBGAs/s400/character_nue.png"  width=auto height="50%">',
-  '<img src="https://2.bp.blogspot.com/-VhHMAZ8DaJo/WdyDWty_RjI/AAAAAAABHb0/dt3MgtVS_XYsIjwhnvdFus8NGl8TDSc-wCLcBGAs/s400/fantasy_manticore.png"  width=auto height="50%">',
-  '<img src="https://4.bp.blogspot.com/-j_DQJHL6P7Q/WCqrsS08a9I/AAAAAAAA_tg/ScJNzIFkHsUeFK9A_1Xdp9Ogy5jg8Rk7QCLcB/s400/fantasy_baku.png"  width=auto height="50%">',
+  '<img src="https://1.bp.blogspot.com/-Zg3Rhwl3Np4/VuKENvWUmLI/AAAAAAAA4x8/ltMQ0pH0zR071jtfl8sG7mS3SQTJdh0iQ/s800/uma_chupacabra_new.png"  width=auto height="50vw">',
+  '<img src="https://3.bp.blogspot.com/-GRGQEnE3UqA/UT10SkjkLMI/AAAAAAAAOvE/3vk4npeqTFg/s400/youkai_kirin.png"  width=auto height="50vw">',
+  '<img src="https://3.bp.blogspot.com/-efiP_VuiSs8/WvkK6D5maMI/AAAAAAABMCY/-3dDSL_pTkc5LiuUnu0EEgs3TL4be1bpgCPcBGAYYCw/s800/fantasy_yamata_red.png"  width=auto height="50vw">',
+  '<img src="https://4.bp.blogspot.com/-6p79nzYZn-Q/UT10LFSwRXI/AAAAAAAAOss/X6_1oWkCecs/s450/fantasy_yamata.png"  width=auto height="50vw">',
+  '<img src="https://4.bp.blogspot.com/-gKPQ9hjH4Vs/UT10IwHX8nI/AAAAAAAAOsE/Hesj5Cuhs4o/s400/fantasy_ogre.png"  width=auto height="50vw">',
+  '<img src="https://3.bp.blogspot.com/-zs4Ab_aVoac/UT10H2mvixI/AAAAAAAAOr8/skuRZVt4nDQ/s400/fantasy_goblin.png"  width=auto height="50vw">',
+  '<img src="https://3.bp.blogspot.com/-i-FxBmcAJg4/UT10Kvl0MJI/AAAAAAAAOsk/knkH22IM-DQ/s400/fantasy_wolfman.png"  width=auto height="50vw">',
+  '<img src="https://4.bp.blogspot.com/-yr7B9IanNcY/Wn1V29LFeVI/AAAAAAABKF4/jUBITNcwgXE4QpD4coaOyE7HngsxND2RwCLcBGAs/s400/fantasy_mandrake_mandragora.png"  width=auto height="50vw">',
+  '<img src="https://3.bp.blogspot.com/-LKAxA70lNJc/WUdYUs_hdwI/AAAAAAABE6c/zIdhHJeM_k0zUGE-lrpwCOHcx2H9XPLrgCLcBGAs/s400/character_nue.png"  width=auto height="50vw">',
+  '<img src="https://2.bp.blogspot.com/-VhHMAZ8DaJo/WdyDWty_RjI/AAAAAAABHb0/dt3MgtVS_XYsIjwhnvdFus8NGl8TDSc-wCLcBGAs/s400/fantasy_manticore.png"  width=auto height="50vw">',
+  '<img src="https://4.bp.blogspot.com/-j_DQJHL6P7Q/WCqrsS08a9I/AAAAAAAA_tg/ScJNzIFkHsUeFK9A_1Xdp9Ogy5jg8Rk7QCLcB/s400/fantasy_baku.png"  width=auto height="50vw">',
 ];
 var MAJUNAME=[
   "チュパカブラ","麒麟","ヤマタノオロチ","ヤマタノオロチ","オーガ","ゴブリン","狼男","マンドレイク","鵺","マンティコア","貘"
